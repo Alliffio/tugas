84c28f421c2f633b33dc619ed47aa8f7d6e2a502
Alliffio <55769866+Alliffio@users.noreply.github.com>
2020-04-09 17:32:35 +0700

    Delete aboutme.php

--- aboutme.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo "Nama : Alliffio <br>" ;
-    echo "Kelas : XI RPL 1 <br>";
-    echo "Asal Sekolah : SMK Taruna Bhakti Depok <br>";
-    echo "Alamat : Kp. Sindangkarsa RT 05 RW 15 No.31, Tapos, Depok <br>"
-?>
