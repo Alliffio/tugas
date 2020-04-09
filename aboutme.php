268085b0765d1be236b317af9ceb1e52b682924d
Alliffio <55769866+Alliffio@users.noreply.github.com>
2020-04-09 16:57:19 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Nama : Alliffio <br>" ;
+    echo "Kelas : XI RPL 1 <br>";
+    echo "Asal Sekolah : SMK Taruna Bhakti Depok <br>";
+    echo "Alamat : Kp. Sindangkarsa RT 05 RW 15 No.31, Tapos, Depok <br>"
+?>
