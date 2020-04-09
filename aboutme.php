a570f69678c783f34773987b17a3301a0d6cfa09
Alliffio <55769866+Alliffio@users.noreply.github.com>
2020-04-09 16:37:43 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Nama : Alliffio <br>" ;
+    echo "Kelas : XI RPL 1 <br>";
+    echo "Asal Sekolah : SMK Taruna Bhakti Depok <br>";
+    echo "No HP : 089630214234 <br>";
+    echo 'Email : aliffio1615@gmail.com <br>';
+?>
